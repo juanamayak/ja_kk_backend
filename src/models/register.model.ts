f547aceecffb7ae826a2d3c5d343832537648640
Juan Amaya <amayajuan95@gmail.com>
2022-07-03 15:22:01 -0500

    Fix #3 - Se termino proceso de validacion y registro

--- src/models/register.model.ts
+++ src/models/register.model.ts
@@ -6,19 +6,25 @@ export class RegisterModel extends Model {
     public kid_name: any;
     public kid_birthday: any;
     public kid_age: any;
-    public is_kid_allergy: any;
-    public kid_allergy_description: any;
+    public studying_level: any;
     public father_name: any;
     public father_cellphone: any;
     public mother_name: any;
     public mother_cellphone: any;
-    public phone: any;
     public address: any;
+    public phone: any;
+    public ap_name_one: any;
+    public ap_relationship_one: any;
+    public ap_cellphone_one: any;
+    public ap_name_two: any;
+    public ap_relationship_two: any;
+    public ap_cellphone_two: any;
+    public kid_allergy: any;
+    public allergy_description: any;
+    public mdf_member: any;
     public email: any;
-    public authorized_person: any;
-    public is_member: any;
     public invited_by_mdf_member: any;
-    public who_invites_name: any;
+    public inviters_name: any;
     public qr_code: any;
     public createdAt: any;
     public updatedAt: any;
@@ -41,17 +47,14 @@ RegisterModel.init({
             allowNull: false
         },
         kid_age: {
-            type: DataTypes.STRING(10),
+            type: DataTypes.STRING(100),
             allowNull: false
         },
-        is_kid_allergy: {
-            type: DataTypes.INTEGER,
+        studying_level: {
+            type: DataTypes.STRING(100),
             allowNull: false
         },
-        kid_allergy_description: {
-            type: DataTypes.STRING(255),
-            allowNull: false
-        },
+
         father_name: {
             type: DataTypes.STRING(150),
             allowNull: false
@@ -68,31 +71,59 @@ RegisterModel.init({
             type: DataTypes.STRING(15),
             allowNull: false
         },
+        address: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
         phone: {
             type: DataTypes.STRING(15),
             allowNull: false
         },
-        address: {
+        ap_name_one: {
+            type: DataTypes.STRING(150),
+            allowNull: true
+        },
+        ap_relationship_one: {
+            type: DataTypes.STRING(150),
+            allowNull: true
+        },
+        ap_cellphone_one: {
+            type: DataTypes.STRING(15),
+            allowNull: true
+        },
+        ap_name_two: {
+            type: DataTypes.STRING(150),
+            allowNull: true
+        },
+        ap_relationship_two: {
+            type: DataTypes.STRING(150),
+            allowNull: true
+        },
+        ap_cellphone_two: {
+            type: DataTypes.STRING(15),
+            allowNull: true
+        },
+        kid_allergy: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        allergy_description: {
             type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        mdf_member: {
+            type: DataTypes.INTEGER,
             allowNull: false
         },
         email: {
             type: DataTypes.STRING(100),
             allowNull: false
         },
-        authorized_person: {
-            type: DataTypes.STRING(150),
-            allowNull: false
-        },
-        is_member: {
+        invited_mdf_member: {
             type: DataTypes.INTEGER,
             allowNull: false
         },
-        invited_by_mdf_member: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        who_invites_name: {
+        inviters_name: {
             type: DataTypes.STRING(150),
             allowNull: false
         },
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,17 +2,24 @@ import {Application} from 'express';
 
 /* Controllers */
 import {RegisterController} from "../controllers/register.controller";
+import {CheckinAndOutController} from "../controllers/checkin_and_out.controller";
 
 /* Middlewares */
 import {validateCreate} from '../validators/register.validator';
 
 export class Routes {
     public registerController: RegisterController = new RegisterController();
+    public checkinAndOutController: CheckinAndOutController = new CheckinAndOutController();
     public routes(app: Application) {
-        /* Rout for kids register */
-        app.route('/api/register').post(this.registerController.register);
+        /* Routes for kids register */
+        app.route('/api/register').post(validateCreate, this.registerController.register);
         app.route('/api/register/:id').get(this.registerController.show);
         app.route('/api/register/qr/:id').get(this.registerController.getQRCodeImage);
         app.route('/api/register/confirmation/:id').get(this.registerController.confirmation);
+
+        /* Routes for check in and out*/
+        app.route('/api/checkinAndOut/:id').get(this.checkinAndOutController.showByRegister);
+        app.route('/api/checkinAndOut/checkin').post(this.checkinAndOutController.checkin);
+        app.route('/api/checkinAndOut/checkout/:id').put(this.checkinAndOutController.checkout);
     }
 }
