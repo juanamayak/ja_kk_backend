9fea8c316c09ec61ccabcd96b7fe67320b5f26c4
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2024-06-27 14:50:17 -0500

    Update server.ts

--- src/config/server.ts
+++ src/config/server.ts
@@ -35,7 +35,7 @@ class Server {
         Relationship.init();
 
         // CORS
-        this.app.use(cors({origin:'https://mundodefeplaya.org'}));
+        this.app.use(cors());
 
         this.app.use(useragent.express())
         this.app.use(helmet())
