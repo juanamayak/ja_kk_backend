e76e6048d9021dd0916e033dfe405a49e1bbabd3
Juan Amaya <amayajuan95@gmail.com>
2024-06-28 00:51:42 -0500

    Se añaden logs para depuración

--- src/config/server.ts
+++ src/config/server.ts
@@ -53,16 +53,17 @@ class Server {
         if (process.env.MODE === 'dev') {
             this.server = http.createServer(this.app);
         } else {
-            // @ts-ignore
             const privateKey = fs.readFileSync(process.env.PRIVATE_SSL, 'utf8')
-            // @ts-ignore
             const certificate = fs.readFileSync(process.env.CERTIFICATE_SSL, 'utf8')
-            // @ts-ignore
             const cabundle = fs.readFileSync(process.env.CABUNDLE_SSL, 'utf8')
 
+            console.log(privateKey)
+            console.log(certificate)
+            console.log(cabundle)
             const credentials = { key: privateKey, cert: certificate, ca: cabundle }
 
             this.server = https.createServer(credentials, this.app)
+            console.log('https ok');
         }
     }
 
